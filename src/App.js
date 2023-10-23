e2588da59ab311e03a6c35c2237fc170d5416583
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-23 16:23:28 -0400

    testing UI lab done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Kyra</h1>
+      <div>
+        <img
+          src="https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png"
+          alt="headshot"
+        />
+        <article>
+          <h2>About Me</h2>
+          <p>This is a paragraph about me.</p>
+          <a href="https://www.linkedin.com/in/kyra-hamerling-potts/">
+            Linkedin
+          </a>
+          <a href="https://github.com/khamerling-potts">Github</a>
+        </article>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+// An image of yourself with alt text identifying the content of the image
+// A second-level heading with the text About Me
+// A paragraph for your biography
+// Two links, one to your GitHub page, and one to your LinkedIn page
